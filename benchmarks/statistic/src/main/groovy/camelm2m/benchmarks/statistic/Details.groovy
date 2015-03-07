d290612075cbeb561daa96f9e26b64903c2aeba8
ajurasz <arkadiusz.jurasz@gmail.com>
2015-03-07 15:23:46 +0100

    Fixed groovy bean

--- benchmarks/statistic/src/main/groovy/camelm2m/benchmarks/statistic/Details.groovy
+++ benchmarks/statistic/src/main/groovy/camelm2m/benchmarks/statistic/Details.groovy
@@ -4,17 +4,17 @@ import static java.lang.management.ManagementFactory.getThreadMXBean
 
 class Details {
 
-    private final Long created;
-    private final Long consumed;
-    private final Long duration;
-    private final Long creationPerformance;
-    private final Long consumptionPerformance;
+    Long created;
+    Long consumed;
+    Long duration;
+    Long creationPerformance;
+    Long consumptionPerformance;
 
     Long getCurrentQueueSize() {
         created - consumed
     }
 
-    Long threadCount() {
+    Long getThreadCount() {
         getThreadMXBean().getThreadCount()
     }
 }
